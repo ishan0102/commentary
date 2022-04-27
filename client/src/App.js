b53ef96610614a54e0e7492d7780f7ab5a6e16a9
xige <mxchen@wireless-10-145-10-10.public.utexas.edu>
2022-04-26 20:27:46 -0500

    Frontend React App

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
+import TwitterIcon from './assets/twitter.svg';
+import axios from 'axios';
 
-function App() {
+const API = 'http://localhost:8000';
+
+export default function App() {
+
+  const tweetBoxRef = useRef(null);
+  const [score, setScore] = useState(0);
+  const [characterLength, setCharacterLength] = useState(0);
+
+  const getColor = (score) => {
+    // using bootstrap's colors since they're similar to twitter's
+    if (score < -0.3) return '#dc3545'; // red
+    if (score > 0.3) return '#198754'; // green
+    return '#1DA1F2'; // twitter blue
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='score'>
+        Score: {score}
+        <div className='scorebar' style={{width: `${(score + 1)/2 * 100}%`, backgroundColor: getColor(score)}}></div>
+      </div>
+      <div className='tweet-compose'>
+        <div className='compose-header'>
+          <img src={TwitterIcon} alt="Twitter"/>
+          Twitter: A Commentary
+        </div>
+        <div className='compose-body'>
+          <textarea 
+            maxLength={280} //  max length of a tweet
+            ref={tweetBoxRef} 
+            onChange={() => {
+              setCharacterLength(tweetBoxRef.current.value.length);
+            }}
+            placeholder="What's happening?">            
+          </textarea>
+        </div>
+        <div className='compose-footer'>
+          {characterLength}
+          <button className='tweet-button' 
+          onClick={() => {
+            // prevent api calls if tweet is too short
+            if (tweetBoxRef.current.value.length < 8) {
+              alert('Tweet must be at least 15 characters long');
+              return
+            }
+            // random percentage between -1 and 1
+            setScore(Math.random() * 2 - 1);
+            axios.post(`${API}/predict`, {
+              'tweet': tweetBoxRef.current.value
+            }).then(res => {
+              console.log(res);
+              setScore((res.data.sentiment * 10));
+            }).catch(err => {
+              console.log(err);
+            })
+
+          }}>
+            Tweet
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
